a5cc6551349823a2a7a0e0e4a4bfe3c7dd85c479
Fernanda Gonçalves de Oliveira Passos <23640770+fgopassos@users.noreply.github.com>
2022-10-11 15:42:29 +0100

    Update X509CertificateDemo.java

--- JCA/X509CertificateDemo.java
+++ JCA/X509CertificateDemo.java
@@ -11,45 +11,46 @@ import java.security.NoSuchAlgorithmException;
 
 public class X509CertificateDemo {
 
-    public static void main(String args[]) throws FileNotFoundException, CertificateException, NoSuchAlgorithmException, InvalidKeySpecException {
+	public static void main(String args[]) throws FileNotFoundException, CertificateException, NoSuchAlgorithmException, InvalidKeySpecException {
 
-        // Assume que ficheiro cert.cer está na diretoria de execução.
-        FileInputStream in = new FileInputStream("cert.cer");
+		// Assume que ficheiro cert.cer está na diretoria de execução.
+		FileInputStream in = new FileInputStream("cert.cer");
 
-        // Gera objeto para certificados X.509.
-        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		// Gera objeto para certificados X.509.
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
 
-        // Gera o certificado a partir do ficheiro.
-        X509Certificate certificate = (X509Certificate) cf.generateCertificate(in);
+		// Gera o certificado a partir do ficheiro.
+		X509Certificate certificate = (X509Certificate) cf.generateCertificate(in);
 
-        // Obtém a chave pública do certificado.
-        PublicKey pk = certificate.getPublicKey();
+		// Obtém a chave pública do certificado.
+		PublicKey pk = certificate.getPublicKey();
 
-        // Converte o objeto pk para RSAPublicKey.
-        KeyFactory factory = KeyFactory.getInstance("RSA");
-        RSAPublicKeySpec pkRSA = factory.getKeySpec(pk, RSAPublicKeySpec.class);
+		// Converte o objeto pk para RSAPublicKey.
+		KeyFactory factory = KeyFactory.getInstance("RSA");
+		RSAPublicKeySpec pkRSA = factory.getKeySpec(pk, RSAPublicKeySpec.class);
 
-        // Mostra informações da chave pública:
-        System.out.println("Algoritmo da chave pública: " + pk.getAlgorithm());
-        System.out.print("Chave pública: ");
-        prettyPrint(pk.getEncoded());
-        System.out.println("Expoente (BigInt): " + pkRSA.getPublicExponent());
-        System.out.println("Modulus (BigInt): " + pkRSA.getModulus());
+		// Mostra informações da chave pública:
+		System.out.println("Algoritmo da chave pública: " + pk.getAlgorithm());
+		System.out.print("Chave pública: ");
+		prettyPrint(pk.getEncoded());
+		System.out.println("Expoente (BigInt): " + pkRSA.getPublicExponent());
+		System.out.println("Modulus (BigInt): " + pkRSA.getModulus());
 
-        // Alguns exemplos de acesso aos campos do certificado:
-        System.out.println("Tipo: " + certificate.getType());
-        System.out.println("Versão: " + certificate.getVersion());
-        System.out.println("Algoritmo de assinatura: " + certificate.getSigAlgName());
-        System.out.println("Período: " + certificate.getNotBefore() + " a " + certificate.getNotAfter());
-        System.out.print("Assinatura: ");
-        prettyPrint(certificate.getSignature());
+		// Alguns exemplos de acesso aos campos do certificado:
+		System.out.println("Tipo: " + certificate.getType());
+		System.out.println("Versão: " + certificate.getVersion());
+		System.out.println("Algoritmo de assinatura: " + certificate.getSigAlgName());
+		System.out.println("Período: " + certificate.getNotBefore() + " a " + certificate.getNotAfter());
+		System.out.print("Assinatura: ");
+		prettyPrint(certificate.getSignature());
 
-        // Verifica a validade do período do certificado.
-        certificate.checkValidity();
-        System.out.println("Certificado válido (período de validade)");
-    }
-
-  	// Imprime array de bytes em hexadecimal
+		// Verifica a validade do período do certificado.
+		certificate.checkValidity();
+		System.out.println("Certificado válido (período de validade)");
+		
+	}
+	
+	// Imprime array de bytes em hexadecimal
 	private static void prettyPrint(byte[] tag) {
 		for (byte b: tag) {
 			System.out.printf("%02x", b);
